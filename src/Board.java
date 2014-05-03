c963288fbb3c472016abf337b51ae280e6981294
Victor Jiao <VjiaoBlack@gmail.com>
2014-05-03 09:54:13 -0400

    coollision and stuffkinda notrealy

--- src/Board.java
+++ src/Board.java
@@ -1,40 +1,201 @@
-import javax.swing.*;
+import javax.swing.*; 
+
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferStrategy;
 
 //The file that holds the JFrame.
 
-public class Board{
-	private static JFrame board; //The main frame on which everything is painted on.
-	private static Layering canvas; //The canvas with essentially all fo the paint methods.
-
+public class Board extends Canvas implements MouseListener, KeyListener, MouseMotionListener{
+	// this is a terrain
+	private Terrain _terrain;
+	private Player _player;
+	
 	//Dimensions
-	private static final int width=800,
-						height=400,
-						xLoc=30,
-						yLoc=30;
+	private static final int WIDTH=800,
+						HEIGHT=400,
+						X_LOC=30,
+						Y_LOC=30;
+	// SKIP_TICKS is how much time to wait for each ms.
+	public static final int FPS = 60, SKIP_TICKS = 1000 / FPS;
 
+	// private BufferedImage _background;
+	private boolean _running;
+	
 	public Board(){
-		prepareGui();
-		canvas.repaint();
+		init();
+		addKeyListener(this);
+		addMouseListener(this);
+		addMouseMotionListener(this);
+		loadResources();
+	}
+	
+
+	public void loadResources() {
+		
+	}
+	
+	public void init(){
+		_running = false;	
+		_terrain = new Terrain();
+		_player = new Player();
 	}
 
-	public void prepareGui(){
-		board = new JFrame("Assassin");
-		board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		board.setSize(width,height);
-		board.setLocation(30,30);
-		board.setVisible(true);
-
-		canvas = new Layering();
-
-		board.getContentPane().add(canvas);
+	public void run() {
+		long nextTick = System.currentTimeMillis();
+		long sleepTime = 0;
+		_running = true;
+		createBufferStrategy(2);
+		BufferStrategy strategy = getBufferStrategy();
+		while (_running) {
+			Graphics2D graphics = (Graphics2D) strategy.getDrawGraphics();
+			update(graphics);
+			
+			nextTick += SKIP_TICKS;
+			sleepTime = nextTick - System.currentTimeMillis();
+			if (sleepTime >= 0) {
+				try {
+					Thread.sleep(sleepTime);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			graphics.dispose();
+			strategy.show();
+		}
+	}
+	
+	public void shutdown() {
+		_running = false;
+	}
+	
+	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		
+		g2.setColor(Color.BLACK);
+		_terrain.paint(g2);
+		_player.paint(g2);
+	}
+	
+	public void update(Graphics2D g) {
+		// update stuff
+		_player.update();
+		
+		for (Rectangle rect: _terrain.rectangles) {
+			// collision detecting is broken (heh)
+			// brian go work here
+			if (rect.intersects(_player.getRect())) {
+				// if player is above
+				_player.unFall();
+				_player.setYcor(rect.y - 63);
+				
+				// if terrain is above
+				_player.setYvel(0);
+				// if player is to left
+				_player.stopMoving();
+				// if player is to right
+				_player.stopMoving();
+			}
+			
+		}
+		
+		
+		
+		
+		
+		// double buffer
+		Graphics offgc;
+		Image offscreen = null;
+		offscreen = createImage(1080,640);
+		offgc = offscreen.getGraphics();
+		
+		paint(offgc);
+		
+		g.drawImage(offscreen,0,0,this); // not sure what this line does
 	}
 
-	public static int getWidth(){ return width; }
-	public static int getHeight(){ return height; }
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
-	public static void main(String[] args) {
-		Board b = new Board();
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		switch (e.getKeyCode()) {
+			case KeyEvent.VK_UP:
+				_player.jump();
+				break;
+			case KeyEvent.VK_LEFT:
+				_player.moveLeft();
+				break;
+			case KeyEvent.VK_RIGHT:
+				_player.moveRight();
+				break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		switch (e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+			case KeyEvent.VK_RIGHT:
+				_player.stopMoving();
+				break;
+			case KeyEvent.VK_ESCAPE:
+			case KeyEvent.VK_Q:
+				shutdown();
+				_running = false;
+				break;
+		}
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
---
+++ src/Player.java
@@ -0,0 +1,132 @@
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
+
+public class Player {
+	// this is what we're using for the player graphic
+	private static final int PLAYER_WIDTH = 32, PLAYER_HEIGHT = 64;
+	
+	
+	Rectangle2D _rect;
+	private double _xcor, _ycor, _xvel, _yvel;
+	private boolean _falling;
+	private int _coneDir; //direction of viewing cone in degs
+	
+	public Player() {
+		this(400,200);
+	}
+	public Player(int x, int y) {
+		_rect = new Rectangle2D.Double(x,y,PLAYER_WIDTH,PLAYER_HEIGHT);
+		_xcor = x;
+		_ycor = y;
+		_xvel = 0;
+		_yvel = 0;
+		_coneDir = 0;
+		_falling = true;
+	}
+	
+	public void paint(Graphics2D g2) {
+		g2.setColor(Color.GREEN);
+		g2.fill(_rect);
+	}
+	
+	public void update() {
+		_yvel += .5;
+		
+		_xcor += _xvel;
+		_ycor += _yvel;
+		
+		_rect.setFrame(_xcor, _ycor,PLAYER_WIDTH,PLAYER_HEIGHT);
+	}
+	
+	public void jump() {
+		if (!_falling) {
+			_yvel = -1;
+			_falling = true;
+		}
+	}
+	
+	public void moveLeft() {
+		_xvel = -5;
+		
+	}
+	
+	public void stopMoving() {
+		_xvel = 0;
+	}
+	
+	public void moveRight() {
+		_xvel = 5;
+	}
+	
+	public boolean isFalling(){
+		return _falling;
+	}
+	
+	public boolean fall() {
+		boolean buf = _falling;
+		_falling = true;
+		return buf;
+	}
+	
+	public boolean unFall() {
+		boolean buf = _falling;
+		_falling = false;
+		return buf;
+	}
+	
+	public Rectangle getRect() {
+		Rectangle rect = new Rectangle((int)_xcor,(int)_ycor,PLAYER_WIDTH,PLAYER_HEIGHT);
+		return rect;
+		
+	}
+	
+	public double getXcor() {
+		return _xcor;
+		
+	}
+	public double getYcor() {
+		return _xcor;
+		
+	}
+	public double getXvel() {
+		return _xcor;
+		
+	}
+	public double getYvel() {
+		return _xcor;
+		
+	}
+	public int getConeDir() {
+		return _coneDir;
+		
+	}
+	public double setXcor(double x) {
+		double buf = _xcor;
+		_xcor = x;
+		return buf;
+		
+	}
+	public double setYcor(double y) {
+		double buf = _ycor;
+		_ycor = y;
+		return buf;
+	}
+	public double setXvel(double xvel) {
+		double buf = _xvel;
+		_xvel = xvel;
+		return buf;
+		
+	}
+	public double setYvel(double yvel) {
+		double buf = _yvel;
+		_yvel = yvel;
+		return buf;
+	}
+	public int setConeDir(int dir) {
+		int buf = _coneDir;
+		_coneDir = dir;
+		return buf;
+		
+	}
+}
