d5e160a0a69fa1b18a983ace6e2795b1c27ff293
Brian Chuk <brian_chuk@yahoo.com>
2014-05-03 02:08:03 -0400

    entity

---
+++ Collision/entities/Entity.java
@@ -0,0 +1,11 @@
+package edu.stuy.blackout.entities;
+
+import java.awt.Color;
+import java.awt.*;
+import java.util.*;
+
+public class Entity extends Rectangle{
+
+
+
+}
--- Collision/entities/Platform.java
+++ Collision/entities/Platform.java
@@ -4,17 +4,7 @@ import java.awt.Color;
 import java.awt.*;
 import java.util.*;
 
-public class Player extends Rectangle{
+public class Platform extends Rectangle{
 	private static final int height = 20;
 	private static final int width = 10;
-
-
-
-
-
-
-
-
-
-
 }
