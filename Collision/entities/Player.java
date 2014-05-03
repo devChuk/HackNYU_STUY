960b9b09c84e44bb1d0ac791b9c70943d121c397
Brian Chuk <brian_chuk@yahoo.com>
2014-05-03 01:04:34 -0400

    adding stuff

---
+++ Collision/entities/Player.java
@@ -0,0 +1,10 @@
+package edu.stuy.blackout.entities;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+
+public class Player {
+	private static final int height = 20;
+	private static final int width = 10;
+}
